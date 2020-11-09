82956a09ca5fd4845d05c5b20ac46361fa5886b8
子旭 苏 <fdream.lhl@foxmail.com>
2020-11-09 16:22:11 +0800

    PHP 7.4 new feature. Fix timer clear bug.

--- src/RedisPool.php
+++ src/RedisPool.php
@@ -1,11 +1,9 @@
 <?php
 namespace Swango\Cache;
-/**
- *
- * @author fdrea
- */
 final class RedisPool {
-    private static $config, $queue, $timer_isset = false;
+    private static array $config;
+    private static \SplQueue $queue;
+    private static int $timer;
     public static function initInWorker() {
         self::$config = \Swango\Environment::getFrameworkConfig('redis');
         self::$queue = new \SplQueue();
@@ -13,34 +11,38 @@ final class RedisPool {
     private static function newConnection(): \Swoole\Coroutine\Redis {
         $connection = new \Swoole\Coroutine\Redis();
         $connection->connect(self::$config['host'], self::$config['port']);
-
-        if (! self::$timer_isset) {
-            self::$timer_isset = true;
-            \swoole_timer_after(rand(50, 5000), '\\swoole_timer_tick', 10000,
-                '\\Swango\\Cache\\RedisPool::checkConnection');
+        if (! isset(self::$timer)) {
+            self::$timer = \swoole_timer_after(mt_rand(50, 5000), function () {
+                self::$timer = \swoole_timer_tick(10000, '\\Swango\\Cache\\RedisPool::checkConnection');
+            });
         }
-
         return $connection;
     }
     public static function push(\Swoole\Coroutine\Redis $db): void {
         // 因为各种原因，push失败了，要抛弃该条连接，总连接数减1
-        if ($db->connected)
+        if ($db->connected) {
             self::$queue->push($db);
+        }
     }
     public static function pop(): \Swoole\Coroutine\Redis {
         // 如果通道为空，则试图创建，若已达到最大连接数，则注册消费者，等待新的连接
         do {
-            if (self::$queue === null || self::$queue->isEmpty())
+            if (self::$queue === null || self::$queue->isEmpty()) {
                 return self::newConnection();
+            }
             $db = self::$queue->pop();
-        } while ( ! $db->connected );
+        } while (! $db->connected);
         return $db;
     }
     public static function clearQueue(): void {
         self::$queue = new \SplQueue();
+        if (isset(self::$timer)) {
+            \swoole_timer_clear(self::$timer);
+        }
     }
     public static function checkConnection() {
-        if (self::$queue->count() > 2)
+        if (self::$queue->count() > 2) {
             self::$queue->pop()->close();
+        }
     }
 }
